fee4f0daee3f13ad70260f2e7c4e289e5b950721
Михаил Гольдфарб <goldfarbm@mail.ru>
2025-04-14 02:09:10 +0500

    bober+bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+ using UnityEngine;
+
+ public class Bullet : MonoBehaviour
+ {
+     public float speed = 10f;
+     public float lifeTime = 2f;
+     private Rigidbody2D rb;
+
+     void Start()
+     {
+         rb = GetComponent<Rigidbody2D>();
+         rb.linearVelocity = transform.right * speed;
+         Destroy(gameObject, lifeTime);
+     }
+ }
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+     private Rigidbody2D rb;
+    public float speed = 0.5f;
+    private Vector2 moveVector;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveVector.x = Input.GetAxis("Horizontal");
+        moveVector.y = Input.GetAxis("Vertical");
+        rb.MovePosition(rb.position + moveVector * speed * Time.deltaTime);
+    }
+}
